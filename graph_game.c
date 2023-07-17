573df4f5ac0b7176b53be99888e0df41544567ee
SRAVAN-IITB <132607113+SRAVAN-IITB@users.noreply.github.com>
2023-07-18 02:33:20 +0530

    graph_game.c

--- graph_game.c
+++ graph_game.c
@@ -1,52 +1,68 @@
 #include <stdio.h>
-int MAX_Row(int n, int a[][n], int i);
-int MAX_Col(int n, int a[][n], int j);
+#include <stdbool.h>
+void Update_and_Check(int x[], int *wt_ptr, int n, int m, int p);
 int main()
 {
-    int n, m, array[n][n];
+    int n, m, p, x[n], weight[n][m];
     printf("Enter the number of vertices 'n':\n");
     scanf("%d", &n);
     printf("Enter the number of moves 'm':\n");
     scanf("%d", &m);
     printf("Enter the edge weights:\n");
     for (int i = 0; i < n; ++i) {
+        x[i] = 0;
         for (int j = 0; j < n; ++j) {
             if (i == j)
-                array[i][j] = -2147483648;  // (-2^31)
+                weight[i][j] = -2147483648;  // (-2^31)
             else
-                scanf("%d", &array[i][j]);
+                scanf("%d", &weight[i][j]);
         }
     }
-    for (int i = 0; i < n; ++i) {
+    /*for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             if (i == j) {
                 printf("  ");
             }
             else
-                printf("%d ", array[i][j]);
+                printf("%d ", weight[i][j]);
         }
         printf("\n");
-    }
-    for (int node = 0; node < n; node++) {
-        //  Trying to backtrace the sequence of each node-end, don't know how to exactly code it
-    }
+    }*/
+    Update_and_Check(x, weight, n, m, p);
     return 0;
 }
-int MAX_Row(int n, int a[][n], int i)
-{
-    int MAX = 0;
-    for (int j = 0; j < n; ++j) {
-        if (a[i][j] > MAX)
-            MAX = a[i][j];
+
+void Update_and_Check(int x[], int *wt_ptr, int n, int m, int p) {
+    while (m > 0) {
+        int b[n];
+        for (int i = 0; i < n; i++) {
+            b[i] = x[i] * -1;
+        }
+        for (int i = 0; i < n; i++) {
+            int max = -2147483648;
+            for (int j = 0; j < n; j++) {
+                if (max < wt_ptr[n * j + i] - x[j])
+                    max = wt_ptr[n * j + i] - x[j];
+            }
+            b[i] = max;
+        }
+        m -= p / 2;
+        if (p == 2)
+            p = 1;
+        else
+            p = 2;
+        for (int i = 0; i < n; i++) {
+            x[i] = b[i];
+        }
     }
-    return MAX;
-}
-int MAX_Col(int n, int a[][n], int j)
-{
-    int MAX = 0;
-    for (int i = 0; i < n; ++i) {
-        if (a[i][j] > MAX)
-            MAX = a[i][j];
+    bool flag = false;
+    for (int i = 0; i < n; i++) {
+        x[i] = wt_ptr[0 + n * i] - x[i];
+        if (x[i] > 0)
+            flag = true;
     }
-    return MAX;
+    if (flag)
+        printf("Player 1 is the winner!");
+    else
+        printf("Player 2 is the winner!");
 }
